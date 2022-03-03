1de2a350b1627a6957c9792cae2bb8f3cfb12814
AryanKatariya <aryankatariya21@gmail.com>
2022-03-03 21:51:22 +0530

    3-3-22

--- BinaryTree/BinaryTreeNode.py
+++ BinaryTree/BinaryTreeNode.py
@@ -2,46 +2,57 @@ class Node:
     def __init__(self,data):
         self.left = None
         self.right = None
-        self.val = data
+        self.data = data
 
-    def PrintTree(root):
-        if root == None:
-            return
-        print(root.data, end=':')
+def PrintTree(root):
+    if root == None:
+        return
+    print(root.data, end=':')
 
-        if root.left != None:
-            print('L', root.left.data, end=',')
+    if root.left != None:
+        print('L',root.left.data,end=',')
 
-        if root.right != None:
-            print('R', root.right.data, end='')
+    if root.right != None:
+        print('R',root.right.data,end='')
 
-        print()
+    print()
 
-        PrintTree(root.left)
-        PrintTree(root.right)
+    PrintTree(root.left)
+    PrintTree(root.right)
 
-    def tree():
-        rootData = int(input())
-        if rootData == -1:
-            return None
-        root = Node(rootData)
-        leftTree = tree()
-        rightTree = tree()
-        root.left = leftTree
-        root.right = rightTree
-        return root
+def treeInput():
+    rootData = int(input())
+    if rootData == -1:
+        return None
+    root = Node(rootData)
+    leftTree = tree()
+    rightTree = tree()
+    root.left = leftTree
+    root.right = rightTree
+    return root
 
-    def num(root):
-        if root == None:
-            return 0
-        leftCount = num(root.left)
-        rightCount = num(root.right)
-        return 1 + leftCount + rightCount
+def numNodes(root):
+    if root == None:
+        return 0
+    leftCount = num(root.left)
+    rightCount = num(root.right)
+    return 1 + leftCount + rightCount
 
 
 btn1 = Node(1)
-btn2 = Node(4)
+btn2 = Node(2)
 btn3 = Node(3)
+btn4 = Node(4)
+btn5 = Node(5)
+
 
 btn1.left = btn2
 btn1.right = btn3
+btn2.left = btn4
+btn2.right = btn5
+
+PrintTree(btn1)
+
+
+root = tree()
+PrintTree(root)
