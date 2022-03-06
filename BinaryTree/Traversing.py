b0dee683eb90607e1c166301e2f29f87f99ca475
AryanKatariya <aryankatariya21@gmail.com>
2022-03-06 23:44:34 +0530

    6-3-22

---
+++ BinaryTree/Traversing.py
@@ -0,0 +1,48 @@
+'''
+(a) Inorder (Left, Root, Right) : 4 2 5 1 3
+(b) Preorder (Root, Left, Right) : 1 2 4 5 3
+(c) Postorder (Left, Right, Root) : 4 5 2 3 1'''
+
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+def printInorder(root):
+    if root:
+        printInorder(root.left)
+        print(root.data),
+        printInorder(root.right)
+
+
+def printPostorder(root):
+    if root:
+        printPostorder(root.left)
+        printPostorder(root.right)
+        print(root.data)
+
+
+def printPreorder(root):
+    if root:
+        print(root.data),
+        printPreorder(root.left)
+        printPreorder(root.right)
+
+btn1 = Node(1)
+btn2 = Node(2)
+btn3 = Node(3)
+btn4 = Node(4)
+btn5 = Node(5)
+
+
+btn1.left = btn2
+btn1.right = btn3
+btn2.left = btn4
+btn2.right = btn5
+
+printInorder(btn1)
+print()
+printPreorder(btn1)
+print()
+printPostorder(btn1)
