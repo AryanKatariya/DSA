96d984dbc4183f1b820011966e745a626b1df830
AryanKatariya <aryankatariya21@gmail.com>
2022-03-17 23:54:22 +0530

    17-3-22

--- BST-1/Check_is_BST.py
+++ BST-1/Check_is_BST.py
@@ -64,6 +64,35 @@ def isBST(root):
 
     return root
 
+def isBST2(root):
+    if root == None:
+        return 1000000, -1000000, True
+    leftMin, leftMax, isLeftBST = isBST2(root.left)
+    rightMin, rightMax, isRightBST = isBST2(root.right)
+
+    minimum = min(leftMin, rightMin, root.data)
+    maximum = max(leftMax, rightMax, root.data)
+
+    isTreeBST = True
+
+    if root.data <= leftMax or root.data > rightMin:
+        isTreeBST = False
+    if not(isLeftBST) or not(isRightBST):
+        isTreeBST = False
+
+    return minimum, maximum, isTreeBST
+
+def isBST3(root, min_range, max_range):
+    if root == None:
+        return True
+    if root.data < min_range or root.data > max_range:
+        return False
+
+    isLeftWithinConstraints = isBST3(root.left, min_range, root.data -1)
+    isRightWithinConstraints = isBST3(root.right, root.data, max_range)
+
+    return isLeftWithinConstraints and isRightWithinConstraints
+
 def PrintTree(root):
     if root == None:
         return
